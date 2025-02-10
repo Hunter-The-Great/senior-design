a3394311d88376af4f66184de6763c2c21f1602d
Hunter-The-Great <benlowry9@gmail.com>
2025-02-09 20:44:26 -0600

    Initial Commit

---
+++ Senior Design/Senior_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  Senior_DesignApp.swift
+//  Senior Design
+//
+//  Created by Benjamin on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Senior_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Senior DesignTests/Senior_DesignTests.swift
@@ -0,0 +1,17 @@
+//
+//  Senior_DesignTests.swift
+//  Senior DesignTests
+//
+//  Created by Benjamin on 2/9/25.
+//
+
+import Testing
+@testable import Senior_Design
+
+struct Senior_DesignTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
